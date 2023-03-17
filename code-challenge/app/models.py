4a3072930c719ac68aa126a27bc1ed26e07a17a6
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-17 14:00:30 -0400

    remove serializers

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,19 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_serialize import SerializerMixin
 
 db = SQLAlchemy()
 
-class Restaurant(db.Model, SerializerMixin):
+class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
 
-class Pizza(db.Model, SerializerMixin):
+class Pizza(db.Model):
     __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
 
-class RestaurantPizza(db.Model, SerializerMixin):
+class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
